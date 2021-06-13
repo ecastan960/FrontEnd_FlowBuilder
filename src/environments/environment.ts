3b96778cf9bf701c24f77a8fa06921b2c49a556d
Esteban Castaño <32081995+ecastan960@users.noreply.github.com>
2021-06-12 23:43:50 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyA6QMiNqgMQ9xwAyh9jSxAp5EkVJzRY1Ug",
-    authDomain: "empleados-d0eab.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     projectId: "empleados-d0eab",
     storageBucket: "empleados-d0eab.appspot.com",
     messagingSenderId: "487538213772",
-    appId: "1:487538213772:web:a808038165c830a6a64ca9"
+    appId: ""
   }
 };
 
