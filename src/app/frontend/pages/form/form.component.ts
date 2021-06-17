45f74f5083f0a09c98bf371f2ae2cd6a995f0798
ecastan960 <ecastan1@eafit.edu.co>
2021-06-16 20:56:33 -0500

    02|Temporary output fixes

--- src/app/frontend/pages/form/form.component.ts
+++ src/app/frontend/pages/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray } from '@angular/forms';
 import { CellsService } from '../../../services/cells.service';
+
 
 
 @Component({
@@ -9,46 +11,144 @@ import { CellsService } from '../../../services/cells.service';
 })
 export class FormComponent implements OnInit {
 
-  mensaje:string = '';
-  salidas:number = 1;
-  outputs:string[] = [];
+  // message:string = '';
+  activeWindow = this._cell.pageActive;
+  numberOutputs:number = 1;
+  id = '';
+  currentData = this.fb.group({
+    answers: this.fb.array([]),
+    message: ['']
+  })
+  updateData = [];
+  updateAnswer = false;
+  firstTry = true;
 
-  constructor(private _cell: CellsService) { }
+
+
+  constructor(public _cell: CellsService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log(this.activeWindow); 
 
+  }
+
+  get message(){
+    return this.currentData.get('message')
+  }
+
+  get answers(){
+    return this.currentData.get('answers') as FormArray;
   }
 
   actualizar(){
 
-    this._cell.element.attr(['text'],{text:`${this.mensaje}`})
+    this._cell.element.attr(['text'],{text:`${this.message}`})
     
-    this._cell.element.addOutPort(`test`)
-    this._cell.element.removeOutPort(`test`)
-    console.log(this.outputs);
+    
+    // this._cell.element.addOutPort(`test`)
+    // this._cell.element.removeOutPort(`test`)
+    // console.log(this.outputs);
+    // console.log(this._cell.element.attributes.outPorts);
+  }
+
+  addAnswer(){
+    this.updateAnswer = true;
+    console.log('********');
+      console.log(this.answers.value.length);
+    if (this._cell.element.attributes.outPorts.length > 0 && this.firstTry) {
+      for (let i = 0; i < this._cell.element.attributes.outPorts.length; i++) {
+        this.answers.push(this.fb.control(`${this._cell.element.attributes.outPorts[i]}`));
+      }
+      this.firstTry = false;
+    } else {
+      console.log('--------');
+      console.log(this.answers.value.length);
+      this.id = this._cell.element.id;
+    // console.log(this._cell.pageActive); 
+    // console.log(this._cell.element.attributes);
+    // console.log(this.id);
+      if (this._cell.element){
+        if (this.answers.value.length < 4) {
+          this.answers.push(this.fb.control(''));
+        }
+      }
+    }
+    
+
+    
+
+    // this.outputs = this._cell.element.attributes.outPorts;
+    
+    // if (this.outputs.length < 4) {
+    //   this.numberOutputs += 1;
+    //   this.outputs.push(`out${this.numberOutputs}`)
+    //   // this._cell.element.addOutPort(`out${this.salidas}`)
+    //   // this.outputs = this._cell.element.attributes.outPorts;
+    //   console.log(this.outputs);
+    // }
+  }
+  
+  // test(){
+  //     console.log(this._cell.element.attributes.outPorts);
+  //     if (this._cell.element.attributes.outPorts.length > 0) {
+  //       for (let i = 0; i < this._cell.element.attributes.outPorts.length; i++){
+  //         this.answers.push(this.fb.control(`${this._cell.element.attributes.outPorts[i]}`));
+  //       }
+  //     }
+  // }
+
+
+  removeAnswer(){
+    this.answers.removeAt(this.answers.length - 1)
+    // this.numberOutputs = -1;
+
+    
+    // if (this.outputs.length > 1) {
+    //   this._cell.element.removeOutPort(`out${this.numberOutputs}`)
+    //   this.numberOutputs -= 1;
+    //   this.outputs.pop();
+    //   console.log(this.outputs);
+    //   console.log(this._cell.element.attributes.outPorts);
+    // }
+    
+  }
+
+  resetAnswers(){
+    if (this.answers.value.length > 0) {
+      for (let i = this.answers.value.length; i > 0  ; i--){
+        this.answers.removeAt(i - 1);
+      }
+    }
+  }
+
+  update() {
+    // console.log('___-_____');
+    // console.log(this.answers.value);
+    // console.log(this._cell.element.attributes.outPorts);
+    if (this.answers.length < this._cell.element.attributes.outPorts.length) {
+      for (let i = this._cell.element.attributes.outPorts.length;
+        i >  this.answers.length; i--) {
+          this._cell.element.removeOutPort(`${this._cell.element.attributes.outPorts[i-1]}`)
+        }
+    } else {
+      for (let i = 0; i < this.answers.value.length; i++) {
+        if (this.answers.value[i] != '' && this.answers.value[i] != this._cell.element.attributes.outPorts[i]){
+          this._cell.element.addOutPort(`${this.answers.value[i]}`)
+        }
+      }
+    }
+
+    // this.numberOutputs = 1;
+    this._cell.element.attr(['text'],{text:`${this.message?.value}`})
+    console.log(this._cell.pages.length);
+    console.log(this._cell.pages[0].id);
+    
+    this.message?.setValue('')
+    this._cell.pageActive = false
+    this.updateAnswer = false;
+    this.firstTry = true;
+    this.resetAnswers();
+    console.log('/////////');
     console.log(this._cell.element.attributes.outPorts);
-  }
-
-  agregarSalidas(){
-    this.outputs = this._cell.element.attributes.outPorts;
-    
-    if (this.outputs.length < 4) {
-      this.salidas += 1;
-      this._cell.element.addOutPort(`out${this.salidas}`)
-      this.outputs = this._cell.element.attributes.outPorts;
-      console.log(this.outputs);
-    }
-  }
-
-  eliminarSalidas(){
-    
-    if (this.outputs.length > 1) {
-      this._cell.element.removeOutPort(`out${this.salidas}`)
-      this.salidas -= 1;
-      this.outputs.pop();
-      console.log(this.outputs);
-      console.log(this._cell.element.attributes.outPorts);
-    }
-    
   }
 }
--- src/app/services/cells.service.ts
+++ src/app/services/cells.service.ts
@@ -8,6 +8,7 @@ import { any } from 'lodash';
 import { SidebarComponent } from '../frontend/pages/sidebar/sidebar.component';
 import { ModelResponse } from '../models/model-respose';
 import { ThisReceiver } from '@angular/compiler';
+import { FormComponent } from '../frontend/pages/form/form.component';
 declare var require: any;
 const joint = require('../../../node_modules/jointjs/dist/joint.js');
 
@@ -15,6 +16,7 @@ const joint = require('../../../node_modules/jointjs/dist/joint.js');
   providedIn: 'root'
 })
 export class CellsService {
+    pageActive = false;
     cell: any[] = []
     graph = new joint.dia.Graph;
     standard = joint.shapes.standard;
@@ -26,7 +28,20 @@ export class CellsService {
     element:any = {};
     info:any[] = [];
     pages:ModelResponse[] = [];
-    page = {};
+    page:ModelResponse = {
+        type: "",
+        id: "",
+        question: {
+            type: "",
+            content: ""
+        },
+        answers: [
+            {
+                phrase: "",
+                target: ""
+            }
+        ] 
+    };
     answers: any[]=[];
 
     constructor(private firestore: AngularFirestore) { }
@@ -46,6 +61,7 @@ export class CellsService {
             }
         });
         let escala = 1;
+        
 
         paper.on('blank:mousewheel', function(evt:any, x:any, y:any, delta:any) { 
             if (delta < 0) {
@@ -75,7 +91,9 @@ export class CellsService {
         let tools = currentElement.findView(paper);
         tools.addTools(toolsView);
         tools.showTools();
+        this.pageActive = true;
         this.element = currentElement;
+        // console.log(this.action.action);
         return currentElement
         });
     }
@@ -83,10 +101,10 @@ export class CellsService {
     agregarPregunta(){
         let qtion = new joint.shapes.devs.Model({
         position: { x: 10, y: 90 },
-        size: { width: 150, height: 70 },
+        size: { width: 200, height: 70 },
         attrs: { rect: { fill: 'white' }, text: { text:` ${this.question}`, fill: 'black' } },
         inPorts: ['in'],
-        outPorts: ['out1'],
+        outPorts: [],
         ports: {
             groups: {
                 'in': {
@@ -111,16 +129,15 @@ export class CellsService {
         qtion.changeOutGroup({ position: 'bottom' });
         this.page = {type: "pregunta",id: qtion.id};
         this.pages.push(this.page)
-
     }
 
     nuevoProyecto(){
         let start = new joint.shapes.devs.Model({
         position: { x: 10, y: 10 },
-        size: { width: 70, height: 50 },
+        size: { width: 200, height: 50 },
         attrs: { rect: { fill: 'green' }, text: { text:`Start`, fill: 'black' } },
         inPorts: [],
-        outPorts: ['out1'],
+        outPorts: [],
         ports: {
             groups: {
                 'in': {
@@ -150,10 +167,10 @@ export class CellsService {
     nuevoMensaje(){
         let mess = new joint.shapes.devs.Model({
         position: { x: 10, y: 180 },
-        size: { width: 150, height: 70 },
+        size: { width: 200, height: 70 },
         attrs: { rect: { fill: '#33ECFF' }, text: { text:`Mensaje`, fill: 'black' } },
         inPorts: ['in'],
-        outPorts: ['out1'],
+        outPorts: [],
         ports: {
             groups: {
                 'in': {
@@ -183,7 +200,7 @@ export class CellsService {
     finProyecto(){
         let end = new joint.shapes.devs.Model({
         position: { x: 10, y: 450 },
-        size: { width: 70, height: 50 },
+        size: { width: 200, height: 50 },
         attrs: { rect: { fill: 'black' }, text: { text:`End`, fill: 'white' } },
         inPorts: this.outPorts,
         outPorts: [],
@@ -224,30 +241,31 @@ export class CellsService {
 
     
     guardarProyecto(){
-    let jsonObject = this.graph.toJSON();
-    let temp = {};
+        console.log(this.pages);
+    
+        let jsonObject = this.graph.toJSON(); // este objeto es el que guarda la grafica
+        let temp = {};
 
-    for ( let i = 0; i < this.pages.length; i++) {
-        this.pages[i].answers = [];
-        this.answers = [];
-        for (let j = 0; j < jsonObject.cells.length; j++){
-            if (jsonObject.cells[j]['type'] == 'link') {
-                if (jsonObject.cells[j]['source']['id'] == this.pages[i]['id']) {
-                    temp = {"frase":jsonObject.cells[j]['source']['port'],"target":jsonObject.cells[j]['target']['id']};
-                    this.answers.push(temp);
-                }
-            } else 
-            {
-                if (jsonObject.cells[j]['id'] == this.pages[i]['id']) {
-                    this.pages[i].question = {'type':'text','content':jsonObject.cells[j]['attrs']['text']['text']};
+        for ( let i = 0; i < this.pages.length; i++) {
+            this.pages[i].answers = [];
+            this.answers = [];
+            for (let j = 0; j < jsonObject.cells.length; j++){
+                if (jsonObject.cells[j]['type'] == 'link') {
+                    if (jsonObject.cells[j]['source']['id'] == this.pages[i]['id']) {
+                        temp = {"phrase":jsonObject.cells[j]['source']['port'],"target":jsonObject.cells[j]['target']['id']};
+                        this.answers.push(temp);
+                    }
+                } else 
+                {
+                    if (jsonObject.cells[j]['id'] == this.pages[i]['id']) {
+                        this.pages[i].question = {'type':'text','content':jsonObject.cells[j]['attrs']['text']['text']};
+                    }
                 }
             }
+            this.pages[i].answers = this.answers;
         }
-        this.pages[i].answers = this.answers;
-    }
-    console.log(this.pages);
-    this.outputCx = JSON.stringify(this.pages)
-    console.log(this.outputCx);
+        console.log(this.pages);
+        this.outputCx = JSON.stringify(this.pages) // esta es el json que alimenta a cx
+        console.log(this.outputCx);
         }
 }
-
